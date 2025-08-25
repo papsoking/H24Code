26a468012afc2c9c8dd9deda8f6385701d80cd7c
Papso King <papefadiaba728@gmail.com>
2025-08-25 00:59:25 +0000

    modifications sur index.php

--- backend/index.php
+++ backend/index.php
@@ -4,6 +4,8 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
+echo "API is running";
+
 require 'db.php';
 
 $pdo = connectDB();
